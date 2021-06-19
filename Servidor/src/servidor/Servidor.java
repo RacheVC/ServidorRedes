db261ec7bf6f00708a67b9f9025a1d2e5866f461
RacheVC <69211990+RacheVC@users.noreply.github.com>
2021-06-19 16:42:55 -0600

    Servidor sin terminar

---
+++ Servidor/src/servidor/Servidor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import java.io.IOException;
+import servidor.util.Servidor;
+/**
+ *
+ * @author rache
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+   public static void main(String[] args) throws IOException
+    {
+        Servidor serv = new Servidor(); //Se crea el servidor
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+    
+}
